533f9a2f7a60cd45244dc11b511c4c5145928548
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-10-17 16:29:59 -0400

    init commit

---
+++ hw-10.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+struct pokemon {
+    char *name;
+    int hp;
+    struct pokemon *evolvesTo;
+};
+
+void printPokemon(struct pokemon * x){
+    printf("This Pokemon is %s, with a max HP of %d\n", x->name, x->hp);
+}
+
+struct pokemon * allocPokemon(char *name, int hp){
+    struct pokemon *p = malloc(sizeof(struct pokemon));
+    p->name = name;
+    p->hp = hp;
+    return p;
+}
+
+char * randomStringGen(char * string, int length){
+    int i;
+    for (i = 0; i < length - 1; i++){
+        string[i] = 'a' + rand() % 26;
+    }
+    string[length - 1] = '\0';
+    return string;
+}
+
+int main(){
+    srand(time(NULL));
+    int length = rand() % 50 + 2;
+    char name[length];
+    struct pokemon *p = allocPokemon(randomStringGen(name, length), rand());
+    printPokemon(p);
+    return 0;
+}
