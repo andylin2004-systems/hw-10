f9fde0a487c2dbaa58ed7550ad706a29c264dd5e
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-10-26 11:10:25 -0400

    fix broken print algo

--- hw-10.c
+++ hw-10.c
@@ -3,75 +3,96 @@
 #include <time.h>
 #include <string.h>
 
-struct pokemon {
+struct pokemon
+{
     char *name;
     int hp;
     struct pokemon *evolvesTo;
 };
 
-void printPokemon(struct pokemon * x){
+void printPokemon(struct pokemon *x)
+{
     printf("This Pokemon is %s, with a max HP of %d\n", x->name, x->hp);
 }
 
-struct pokemon * allocPokemon(char *name, int hp){
+struct pokemon *allocPokemon(char *name, int hp)
+{
     struct pokemon *p = malloc(sizeof(struct pokemon));
     p->name = name;
     p->hp = hp;
+    p->evolvesTo = NULL;
     return p;
 }
 
-char * randomStringGen(char * string, int length){
+char *randomStringGen(char *string, int length)
+{
     int i;
-    for (i = 0; i < length - 1; i++){
+    for (i = 0; i < length - 1; i++)
+    {
         string[i] = 'a' + rand() % 26;
     }
     string[length - 1] = '\0';
     return string;
 }
 
-void print_list(struct pokemon * p){
-    printPokemon(p);
-    if (p->evolvesTo){
-        print_list(p->evolvesTo);
+void print_list(struct pokemon *p)
+{
+    if (p)
+    {
+        printPokemon(p);
+        if (p->evolvesTo)
+        {
+            print_list(p->evolvesTo);
+        }
     }
 }
 
-struct pokemon * insert_front(struct pokemon * y, char* name, int hp){
+struct pokemon *insert_front(struct pokemon *y, char *name, int hp)
+{
     struct pokemon *new = allocPokemon(name, hp);
     new->evolvesTo = y;
     return new;
 }
 
-struct pokemon * free_list(struct pokemon * paulAbandonedMe){
-    while (paulAbandonedMe){
-        struct pokemon *next = paulAbandonedMe->evolvesTo;
+struct pokemon *free_list(struct pokemon *paulAbandonedMe)
+{
+    struct pokemon *next;
+    while (paulAbandonedMe)
+    {
+        next = paulAbandonedMe->evolvesTo;
         free(paulAbandonedMe);
         paulAbandonedMe = next;
     }
     return paulAbandonedMe;
 }
 
-struct pokemon * remove_node(struct pokemon *front, char* name){
+struct pokemon *remove_node(struct pokemon *front, char *name)
+{
     struct pokemon *prev = front;
     struct pokemon *current = front;
-    struct pokemon *next = front->evolvesTo;
+    struct pokemon *next = current->evolvesTo;
     while (current)
     {
         printf("check %s", current->name);
         if (strcmp(current->name, name) == 0)
         {
             prev->evolvesTo = next;
-            if (current == front){
+            if (prev == front)
+            {
                 front = current->evolvesTo;
+                free(current);
             }
-            free(current);
+            else
+            {
+                prev->evolvesTo = next;
+            }
             return front;
         }
         else
         {
             prev = current;
-            current = current->evolvesTo;
-            next = current->evolvesTo;
+            current = next;
+            next = next->evolvesTo;
         }
     }
     return front;
--- main.c
+++ main.c
@@ -7,12 +7,16 @@
 int main(){
     struct pokemon *pikachu = allocPokemon("Pikachu", 40);
     pikachu->evolvesTo = allocPokemon("Raichu", 80);
-    struct pokemon *pichu = insert_front(pikachu, "Pichu", 20);
-    print_list(pichu);
-    free_list(pichu);
+    pikachu = insert_front(pikachu, "pichu", 20);
+    print_list(pikachu);
+    free_list(pikachu);
+    printf("oof\n");
     pikachu = allocPokemon("Pikachu", 40);
     pikachu->evolvesTo = allocPokemon("Raichu", 80);
-    pichu = insert_front(pikachu, "Pichu", 20);
-    remove_node(pichu, "Raichu");
-    print_list(pichu);
+    pikachu = insert_front(pikachu, "pichu", 20);
+    pikachu = remove_node(pikachu, "Raichu");
+    print_list(pikachu);
+    pikachu = free_list(pikachu);
+    // printPokemon(pikachu->evolvesTo);
+    print_list(pikachu);
 }
